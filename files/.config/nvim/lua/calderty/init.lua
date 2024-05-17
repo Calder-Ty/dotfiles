83933ab43edb1c8223ff4b95c6eed391c3b9c03c
Tyler Calder <tyler.calder@advancedmd.com>
2024-05-17 10:58:22 -0600

    feat: Move to using lualine

--- files/.config/nvim/lua/calderty/init.lua
+++ files/.config/nvim/lua/calderty/init.lua
@@ -1,6 +1,6 @@
 require("calderty.sets")
 require("lazy").setup("calderty.plugins")
 require("calderty.remaps") -- General Remaps
-require("calderty.statusline")
+-- require("calderty.statusline")
 require("calderty.tabline")
 require("calderty.lsp")
---
+++ files/.config/nvim/lua/calderty/plugins/lualine_themes/monocle.lua
@@ -0,0 +1,34 @@
+colors = require("monocle").colors
+
+return {
+	normal = {
+		a = {bg = colors.shaddow, fg = colors.blue, gui = 'bold'},
+		b = {bg = colors.black, fg = colors.darkblue},
+		c = {bg = colors.none, fg = colors.green}
+	},
+	insert = {
+		a = {bg = colors.shaddow, fg = colors.darkred, gui = 'bold'},
+		b = {bg = colors.black, fg = colors.lightmagenta},
+		c = {bg = colors.none, fg = colors.green}
+	},
+	visual = {
+		a = {bg = colors.black, fg = colors.yellow, gui = 'bold'},
+		b = {bg = colors.black, fg = colors.lightyellow},
+		c = {bg = colors.none, fg = colors.green}
+	},
+	replace = {
+		a = {bg = colors.shaddow, fg = colors.darkred, gui = 'bold'},
+		b = {bg = colors.black, fg = colors.lightmagenta},
+		c = {bg = colors.none, fg = colors.green}
+	},
+	command = {
+		a = {bg = colors.black, fg = colors.darkgreen, gui = 'bold'},
+		b = {bg = colors.black, fg = colors.seagreen},
+		c = {bg = colors.none, fg = colors.green}
+	},
+	inactive = {
+		a = {bg = colors.none, fg = colors.cobalt, gui = 'bold'},
+		b = {bg = colors.none, fg = colors.cobalt},
+		c = {bg = colors.none, fg = colors.cobalt}
+	}
+}
