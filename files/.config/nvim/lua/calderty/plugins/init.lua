8bbbc8759b2db46e69fd8b3aec01eb78e9cce5e3
Tyler Calder <tyler.calder@advancedmd.com>
2026-07-24 13:43:10 -0600

    nvim: Add oil plugin

--- files/.config/nvim/lua/calderty/plugins/init.lua
+++ files/.config/nvim/lua/calderty/plugins/init.lua
@@ -1,9 +1,9 @@
 -- Themes: 
 vim.pack.add({
-	"https://github.com/rebelot/kanagawa.nvim",
 	"https://github.com/tpope/vim-dadbod",
 	"https://github.com/kristijanhusak/vim-dadbod-ui",
 	"https://github.com/folke/todo-comments.nvim",
+    "https://github.com/stevearc/oil.nvim",
 })
 
 local todo = require("todo-comments")
@@ -20,36 +20,16 @@ todo.setup({
 	}
 })
 
-local kanagawa =  require("kanagawa")
-
-kanagawa.setup({
-	colors = {
-		theme = {
-			all = {
-				ui = {
-					bg_gutter = "none"
-				}
-			}
-		},
+-- oil
+require("oil").setup({
+	columns = {
+		"icon",
+		"permission",
+		"size",
+		"mtime",
 	},
-	background = {
-		dark = "dragon"
-	},
-	overrides = function(colors)
-	  local theme = colors.theme
-	  local makeDiagnosticColor = function(color)
-		local c = require("kanagawa.lib.color")
-		return { fg = color, bg = c(color):blend(theme.ui.bg, 0.95):to_hex() }
-	  end
-
-	  return {
-		DiagnosticVirtualTextHint  = makeDiagnosticColor(theme.diag.hint),
-		DiagnosticVirtualTextInfo  = makeDiagnosticColor(theme.diag.info),
-		DiagnosticVirtualTextWarn  = makeDiagnosticColor(theme.diag.warning),
-		DiagnosticVirtualTextError = makeDiagnosticColor(theme.diag.error),
-	  }
-	end
 })
+
 vim.cmd("colorscheme minima")
 -- Plenary: Required by Telescope
 vim.pack.add({{src="https://github.com/nvim-lua/plenary.nvim"}})
